c310fdd672327b6e94e68ec7e28a3177eb76e5d3
Brandon Roberts <robertsbt@gmail.com>
2024-10-29 10:25:59 -0500

    chore: configure CSR-only with rxjs timer in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-csr-api';
+
+  ngOnInit() {
+    timer(0, 1000).subscribe(console.log)
+  }
 }
